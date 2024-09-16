00f4cab46d7e1fc597f31e3ab33dd4d4c77c231a
anastanvir <anas.seoalchemist@gmail.com>
2024-09-16 13:08:57 -0700

    First Commit

---
+++ app/components/Sidebar/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+export function Sidebar () {
+    return (
+        <div>
+            <div>
+                <h2 className='logo text-[1.8rem] bg-gradient-to-r from-white to-gray-500 text-transparent bg-clip-text font-bold w-[200px]'>PAGEBUZZ</h2>
+            </div>
+        </div>
+    )
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,15 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { Sidebar } from "./components/Sidebar";
 
 const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
+  src: "./fonts/Poppins-Regular.woff",
   variable: "--font-geist-sans",
   weight: "100 900",
 });
 const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
+  src: "./fonts/Poppins-Regular.woff",
   variable: "--font-geist-mono",
   weight: "100 900",
 });
@@ -28,6 +29,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <div>
+          <Sidebar/>
+        </div>
         {children}
       </body>
     </html>
